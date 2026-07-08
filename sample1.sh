b3a6cb07899ca0d94bebfc7d562dc82e852d63e1
EC2 Default User <ec2-user@ip-172-31-33-15.ap-south-1.compute.internal>
2026-07-08 06:02:05 +0000

    updated sample1 script file with date

--- sample1.sh
+++ sample1.sh
@@ -5,5 +5,6 @@ echo "Jenkins Build Successful"
 echo "Current Date : $(date)"
 echo "Current User : $(whoami)"
 echo "Hostname     : $(hostname)"
+echo "Build Triggered at $(date)"
 echo "================================="
 
