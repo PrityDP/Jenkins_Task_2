557759ee2dfae63ba9144f1362c6b2ecb1d4af30
EC2 Default User <ec2-user@ip-172-31-33-15.ap-south-1.compute.internal>
2026-07-08 05:10:17 +0000

    Initial Committo sample1.sh

---
+++ sample1.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+echo "================================="
+echo "Jenkins Build Successful"
+echo "Current Date : $(date)"
+echo "Current User : $(whoami)"
+echo "Hostname     : $(hostname)"
+echo "================================="
+
